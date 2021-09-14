a6ce374c407b5a5b5b0a23113288b3da6487d3c5
siva2github <successmcasiva@gmail.com>
2021-09-14 18:04:24 +0000

    change yaml and sh

--- firstproject/publicip.sh
+++ firstproject/publicip.sh
@@ -1,5 +1,1 @@
-testip=`/usr/bin/az vm show -d -g DevOpstestgroup -n testvm --query publicIps -o tsv`
-
 /usr/bin/az vm show -d -g DevOpstestgroup -n testvm --query publicIps -o tsv >> /etc/ansible/hosts
-
-sshpass -p Password@123 ssh-copy-id azureuser@$testip> /dev/null
