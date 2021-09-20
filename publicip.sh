4368dd24bb555c1c7d2d49c85f757c376a5dd1ba
siva2github <successmcasiva@gmail.com>
2021-09-20 12:36:14 +0000

    Updating publicip

--- publicip.sh
+++ publicip.sh
@@ -1,1 +1,7 @@
-/usr/bin/az vm show -d -g DevOpstestgroup -n testvm --query publicIps -o tsv >> /etc/ansible/hosts
+testip=`/usr/bin/az vm show -d -g DevOpstestgroup -n testvm --query publicIps -o tsv`
+FILE="/etc/ansible/hosts"
+if grep -q "$testip" "$FILE" ; then
+        echo 'exist' ;
+else
+        echo $testip >> /etc/ansible/hosts ;
+fi
